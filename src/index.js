3cf463c99c251030f04613a7a17de52292f30659
Nicolas Olmos <nicolas1819991@gmail.com>
2023-01-12 14:03:21 -0300

    error en el registerRootApplicatiobn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import {} from "react-native";
+import AuthNavigation from "./navigation/AuthNavigation";
+
+export default function App() {
+  return <AuthNavigation />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- src/navigation/AuthNavigation.js
+++ src/navigation/AuthNavigation.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import {Auth} from "../src/screens"
+import {Auth} from "../screens"
 const Stack = createNativeStackNavigator();
 
 const AuthNavigation = () => {
